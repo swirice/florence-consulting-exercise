32069c9021cc3b6f8e9c75207f8eac24e1c96c26
swirice <ale.ricu93@gmail.com>
2023-05-01 17:50:26 +0200

    Add markdown

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# florence-consulting-exercise
+### Installation
+
+_Below the steps to follow in order to use the APIs._
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/swirice/florence-consulting-exercise.git
+   ```
+2. Install Docker
+   > https://www.docker.com/products/docker-desktop/
+3. Compile the project and build the .jar
+   ```sh
+   mvn clean install -DskipTests
+   ```
+3. Start the microservice with database embedded as Docker container
+   ```sh
+   docker-compose up -d
+   ```
--- src/main/java/com/exercise/demo/dto/ErrorDTO.java
+++ src/main/java/com/exercise/demo/dto/ErrorDTO.java
@@ -11,20 +11,20 @@ import lombok.Data;
 @Data
 public class ErrorDTO {
 
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime timestamp;
+
 	private HttpStatus httpStatus;
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Europe/Rome")
-	private LocalDateTime timestamp;
+	private String exceptionType;
 
 	private String message;
 
-	private Object details;
-
-	public ErrorDTO(HttpStatus httpStatus, String message, Object details) {
-		this.httpStatus = httpStatus;
+	public ErrorDTO(HttpStatus httpStatus, String exceptionType, String message) {
 		this.timestamp = LocalDateTime.now();
+		this.httpStatus = httpStatus;
+		this.exceptionType = exceptionType;
 		this.message = message;
-		this.details = details;
 	}
 
 }
